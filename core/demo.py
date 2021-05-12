7597d750ea8e407da8e9ae53b7012045e8f7ff07
lynnschmittwilken <l.schmittwilken@tu-berlin.de>
2021-05-12 10:45:47 +0200

    small changes to demo

--- core/demo.py
+++ core/demo.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import main
+import matplotlib
 
 res = main.main()
 
@@ -7,10 +8,14 @@ fig = plt.figure(figsize=[10, 20])
 M = len(res)
 N = len(list(res.values())[0])
 
+matplotlib.rcParams.update({'font.size': 8})
+plt.figure(figsize=(25, 15))
+
 for i, (model_name, model_output) in enumerate(res.items()):
     for j, (stimulus, output) in enumerate(model_output.items()):
-        index = i*N+j+1
-        plt.subplot(M, N, index)
+        index = i+j*M+1
+        plt.subplot(N, M, index)
         plt.title(model_name + " - " + stimulus)
-        plt.imshow(output["image"])
+        plt.imshow(output["image"], cmap='coolwarm')
+        plt.colorbar()
 plt.show()
