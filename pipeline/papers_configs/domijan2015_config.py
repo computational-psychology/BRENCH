142a9bc4bcb7d4a36a3acf7c768099ed43c06dd8
matko031 <matkomatic59@gmail.com>
2021-06-15 14:26:37 +0200

    added domijan2015 script

---
+++ pipeline/papers_configs/domijan2015_config.py
@@ -0,0 +1,35 @@
+import stimuli
+from pipeline.adapters.domijan2015 import main as domijan_main
+from pipeline import main, plots
+
+domijan2015 = {
+    "models": [
+        {
+            "name": "domijan2015",
+            "runner": domijan_main,
+            "model": None,
+            "params": {"S": 20}
+        }
+    ],
+    "stimuli": {
+        "dungeon": stimuli.papers.domijan2015.dungeon(),
+        "cube": stimuli.papers.domijan2015.cube(),
+        "grating": stimuli.papers.domijan2015.grating(),
+        "ring": stimuli.papers.domijan2015.rings(),
+        "bullseye": stimuli.papers.domijan2015.bullseye(),
+        "simultaneous brightness contrast": stimuli.papers.domijan2015.simultaneous_brightness_contrast(),
+        "white": stimuli.papers.domijan2015.white(),
+        "benary_cross": stimuli.papers.domijan2015.benary(),
+        "todorovic": stimuli.papers.domijan2015.todorovic(),
+        "checkerboard contrast-contrast": stimuli.papers.domijan2015.checkerboard_contrast_contrast(),
+        "checkerboard contrast": stimuli.papers.domijan2015.checkerboard(),
+        "checkerboard contrast extended": stimuli.papers.domijan2015.checkerboard_extended()
+    }
+}
+
+def run():
+    res = main.main(domijan2015)
+    plots.plot_outputs(res)
+
+if __name__ == "__main__":
+    run()
