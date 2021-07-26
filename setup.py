e9f50615b60dd8adc03229ef71f2c8d5600c1a80
Joris Vincent <joris.vincent@gmail.com>
2021-07-26 10:16:34 +0200

    Add requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup, find_packages
 
-setup(name='pipeline',
-      version='0.1',
-      description='Brightness models pipeline',
-      author='Matko Matic',
-      author_email='matko.matic@tutanota.com',
-      license='MIT',
-#      packages=['pipeline'],
-      packages=find_packages(include=['pipeline', 'pipeline.*']),
-      zip_safe=False)
+setup(
+    name="pipeline",
+    version="0.1",
+    description="Brightness models pipeline",
+    author="Matko Matic",
+    author_email="matko.matic@tutanota.com",
+    license="MIT",
+    #      packages=['pipeline'],
+    packages=find_packages(include=["pipeline", "pipeline.*"]),
+    zip_safe=False,
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "weasyprint",
+    ],
+)
