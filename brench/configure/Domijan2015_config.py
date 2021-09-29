6d1d7863f9d8ee98f27cd5f6b3599a331aea2b31
Joris Vincent <joris.vincent@gmail.com>
2021-09-29 16:23:18 +0200

    Streamline and verbose configs

--- brench/configure/Domijan2015_config.py
+++ brench/configure/Domijan2015_config.py
@@ -1,6 +1,13 @@
-from pathlib import Path
+# Models
 from brench.utils.adapters import domijan2015
+
+# Stimuli
+import stimuli.papers.domijan2015 as domijan_stimuli
+
+# Run
 import brench.run
+
+# Evaluate
 from brench.evaluate import (
     calculate_targets_difference,
     # FIXME create_RHS_table,
@@ -8,13 +15,10 @@ from brench.evaluate import (
     save_plot,
 )
 
-import stimuli.papers.domijan2015 as domijan_stimuli
 
-# If existent, load model outputs:
-load_pickle = True
-# Save model outputs and evaluation results:
-save_pickle = True
-# Output dir for all outputs:
+# Where does this config save, by default?
+from pathlib import Path
+
 output_dir = Path(__file__).parents[2] / "data" / "Domijan2015"
 
 
@@ -45,7 +49,6 @@ stimuli = {
 }
 
 
-# Run framework with specified config and evaluation functions:
 def run():
     brench.run(
         models,
@@ -59,24 +62,29 @@ def run():
 
 
 # Define which evaluation steps should be performed for each model individually:
-def evaluate_individual(
-    model_name, stimulus_name, model_output, stim, outputs_dir
-):
+def evaluate_each(model_name, stimulus_name, model_output, stim, outputs_dir):
     # TODO: add '{model_name}-{stimulus_name}' as default out values in all the evaluation functions
+
     # Do the target masks exist?
     if stim.target_mask is not None:
         # Save plots for all model outputs individually in subfolder "plots":
+        plot_file = outputs_dir / "plots" / f"{model_name}-{stimulus_name}.png"
         save_plot(
             model_output["image"],
-            out=outputs_dir / "plots" / f"{model_name}-{stimulus_name}.png",
+            out=plot_file,
         )
+        print(f"    saved plot {plot_file}")
 
         # Calculate and save the difference in estimated target intensity individually in "diffs"
+        diff_file = (
+            outputs_dir / "diffs" / f"{model_name}-{stimulus_name}.pickle"
+        )
         calculate_targets_difference(
             model_output["image"],
             stim.target_mask,
-            out=outputs_dir / "diffs" / f"{model_name}-{stimulus_name}.pickle",
+            out=diff_file,
         )
+        print(f"    saved target differences to {diff_file}")
 
 
 # Define which evaluation step should be performed using all model results:
@@ -85,19 +93,45 @@ def evaluate_all(outputs_dir):
     This function assumes all values are saved in files with format "{model_name}-{stimulus_name}"
     """
     # Create an overview plot with all model outputs for the different stimuli:
-    plot_all_outputs(
-        outputs_dir / "plots", outputs_dir / "all_model_outputs.png"
-    )
+    combined_plots = outputs_dir / "all_model_outputs.png"
+    plot_all_outputs(outputs_dir / "plots", combined_plots)
+    print(f"Saved combined figure as {combined_plots}")
 
     # Create table with mean target differences for all models and stimuli:
     # TODO: fix bug with "None" target masks (probably when there more than two target mask values)
-#    create_RHS_table(
-#        outputs_dir / "diffs",
-#        outputs_dir / "target_differences.csv",
-#        normalized=False,
-#    )
-    pass
+    # table_file = outputs_dir / "target_differences.csv"
+    # create_RHS_table(
+    #     outputs_dir / "diffs",
+    #     table_file,
+    #     normalized=True,
+    # )
+    # print(f"Saved table of target differences to {table_file}")
 
 
+# Run from the command-line
 if __name__ == "__main__":
-    res = run()
+    import time
+
+    # If existent, load model outputs:
+    load_pickle = True
+    # Save model outputs and evaluation results:
+    save_pickle = True
+
+    start = time.time()
+
+    # Run framework with specified config and evaluation functions:
+    brench.run(
+        models,
+        stimuli,
+        evaluate_each,
+        evaluate_all,
+        outputs_dir=output_dir,
+        load=load_pickle,
+        save=save_pickle,
+    )
+
+    stop = time.time()
+    print(
+        "All done! Elapsed time:"
+        f" {((stop-start)/60):2.0f}m:{((stop-start) % 60):2.0f}s"
+    )
--- brench/configure/RHS2007_config.py
+++ brench/configure/RHS2007_config.py
@@ -1,6 +1,14 @@
-from pathlib import Path
-import brench.run
+# Models
 from brench.utils.adapters import ODOG_RHS2007, LODOG_RHS2007, FLODOG_RHS2007
+
+# Stimuli
+import stimuli.papers.RHS2007 as RHS_stimuli
+
+
+# Run
+import brench.run
+
+# Evaluate
 from brench.evaluate import (
     calculate_targets_difference,
     create_RHS_table,
@@ -8,13 +16,9 @@ from brench.evaluate import (
     save_plot,
 )
 
-import stimuli.papers.RHS2007 as RHS_stimuli
+# Where does this config save, by default?
+from pathlib import Path
 
-# If existent, load model outputs:
-load_pickle = True
-# Save model outputs and evaluation results:
-save_pickle = True
-# Output dir for all outputs:
 output_dir = Path(__file__).parents[2] / "data" / "RHS2007"
 
 
@@ -63,39 +67,30 @@ stimuli = {
     "checkerboard209": RHS_stimuli.checkerboard209,
 }
 
-
-# Run framework with specified config and evaluation functions:
-def run():
-    brench.run(
-        models,
-        stimuli,
-        evaluate_individual,
-        evaluate_all,
-        outputs_dir=output_dir,
-        load=load_pickle,
-        save=save_pickle,
-    )
-
-
 # Define which evaluation steps should be performed for each model individually:
-def evaluate_individual(
-    model_name, stimulus_name, model_output, stim, outputs_dir
-):
+def evaluate_each(model_name, stimulus_name, model_output, stim, outputs_dir):
     # TODO: add '{model_name}-{stimulus_name}' as default out values in all the evaluation functions
+
     # Do the target masks exist?
     if stim.target_mask is not None:
         # Save plots for all model outputs individually in subfolder "plots":
+        plot_file = outputs_dir / "plots" / f"{model_name}-{stimulus_name}.png"
         save_plot(
             model_output["image"],
-            out=outputs_dir / "plots" / f"{model_name}-{stimulus_name}.png",
+            out=plot_file,
         )
+        print(f"    saved plot {plot_file}")
 
         # Calculate and save the difference in estimated target intensity individually in "diffs"
+        diff_file = (
+            outputs_dir / "diffs" / f"{model_name}-{stimulus_name}.pickle"
+        )
         calculate_targets_difference(
             model_output["image"],
             stim.target_mask,
-            out=outputs_dir / "diffs" / f"{model_name}-{stimulus_name}.pickle",
+            out=diff_file,
         )
+        print(f"    saved target differences to {diff_file}")
 
 
 # Define which evaluation step should be performed using all model results:
@@ -104,19 +99,45 @@ def evaluate_all(outputs_dir):
     This function assumes all values are saved in files with format "{model_name}-{stimulus_name}"
     """
     # Create an overview plot with all model outputs for the different stimuli:
-    plot_all_outputs(
-        outputs_dir / "plots", outputs_dir / "all_model_outputs.png"
-    )
+    combined_plots = outputs_dir / "all_model_outputs.png"
+    plot_all_outputs(outputs_dir / "plots", combined_plots)
+    print(f"Saved combined figure as {combined_plots}")
 
-    # Create overview table with mean target differences for all models and stimuli based on
-    # output for individual evaluation:
-    create_RHS_table(
-        outputs_dir / "diffs",
-        outputs_dir / "target_differences.csv",
-        normalized=True,
-    )
-    pass
+    # Create table with mean target differences for all models and stimuli:
+    # TODO: fix bug with "None" target masks (probably when there more than two target mask values)
+    # table_file = outputs_dir / "target_differences.csv"
+    # create_RHS_table(
+    #     outputs_dir / "diffs",
+    #     table_file,
+    #     normalized=True,
+    # )
+    # print(f"Saved table of target differences to {table_file}")
 
 
+# Run from the command-line
 if __name__ == "__main__":
-    res = run()
+    import time
+
+    # If existent, load model outputs:
+    load_pickle = True
+    # Save model outputs and evaluation results:
+    save_pickle = True
+
+    start = time.time()
+
+    # Run framework with specified config and evaluation functions:
+    brench.run(
+        models,
+        stimuli,
+        evaluate_each,
+        evaluate_all,
+        outputs_dir=output_dir,
+        load=load_pickle,
+        save=save_pickle,
+    )
+
+    stop = time.time()
+    print(
+        "All done! Elapsed time:"
+        f" {((stop-start)/60):2.0f}m:{((stop-start) % 60):2.0f}s"
+    )
